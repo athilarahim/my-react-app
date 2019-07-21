46682b0b69d5da11f197928fbab22232e28328ba
amjadamju <amjadpanjeeri@gmail.com>
2019-07-22 00:12:48 +0530

    fixed errors in app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Navbar dark color = "primary" >
-        <
-        div className = "container" >
-        <
-        NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand>  <
-        /div>        <
-        /Navbar>   <
-        /div>
+    return ( 
+        <div className = "App">
+            <Navbar dark color = "primary">
+                <div className = "container" >
+                    <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand>  
+                </div>        
+            </Navbar>   
+        </div>
     );
 }
 
